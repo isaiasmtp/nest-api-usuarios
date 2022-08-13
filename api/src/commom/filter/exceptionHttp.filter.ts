e95a90954dd737470e5c62a9681ccc2a36bc2f3b
isaiasmtp <isaias.pontes@ccc.ufcg.edu.br>
2022-08-13 11:38:32 -0300

    Create generic http exception

---
+++ api/src/commom/filter/exceptionHttp.filter.ts
@@ -0,0 +1,33 @@
+import { ArgumentsHost, Body, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { AbstractHttpAdapter, HttpAdapterHost } from "@nestjs/core";
+
+@Catch()
+export class ExceptionHttp implements  ExceptionFilter {
+
+    private httpAdapter: AbstractHttpAdapter
+    constructor(adapterHost: HttpAdapterHost){
+        this.httpAdapter = adapterHost.httpAdapter;
+    }
+
+    catch(exception: any, host: ArgumentsHost) {
+        const context = host.switchToHttp();
+        const req = context.getRequest();
+        const res = context.getResponse();
+    
+        const { status, body } = exception instanceof HttpException
+            ? {
+                status: exception.getStatus(),
+                body: exception.getResponse(),
+
+            } :
+            {
+                status: HttpStatus.INTERNAL_SERVER_ERROR, 
+                body: {
+                    statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                    timestamp: new Date().toISOString(),
+                    path: req.path
+                }
+            }
+        this.httpAdapter.reply(res, body, status)
+    }
+}
